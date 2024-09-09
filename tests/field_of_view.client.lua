e57b6202b625700112675096afe3fc3bb2eac209
Amir <139617633+alliancecrusader@users.noreply.github.com>
2024-09-09 21:50:34 +0000

    add field_of_view.client.lua test

---
+++ tests/field_of_view.client.lua
@@ -0,0 +1,65 @@
+--!nonstrict
+
+--------------------------------------------------------------------
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+--------------------------------------------------------------------
+
+-- Place config in replicated storage!
+local config = require(ReplicatedStorage:WaitForChild("config"))
+local base = config.themes.base
+
+--------------------------------------------------------------------
+
+local data = {
+	title = "FOV Changer",
+	objects = {
+		{
+			key = "Field of View",
+			default_value = 60,
+			num_info = {
+				int = true,
+				signed = false,
+				limit = {
+					max = 130,
+					min = 1
+				}
+			}
+		}
+	},
+	requires_apply = false
+}
+
+local function fov_test()
+	local success, config = base.config(data)
+	
+	if not (success and config) then
+		return
+	end
+	
+	local fov : any = config.get_object_from_path("Field of View")
+	
+	fov.changed:Connect(function(value : number)
+		workspace.CurrentCamera.FieldOfView = value
+	end)
+
+	local ScreenGui = Instance.new("ScreenGui")
+	ScreenGui.Parent = game.Players.LocalPlayer.PlayerGui
+	
+	config.frame.get().Parent = ScreenGui
+	config.frame.get().Position = UDim2.fromScale(0.5, 0.5)
+	config.frame.get().Size = UDim2.fromScale(0.3, 0.15)
+end
+
+--------------------------------------------------------------------
+
+local function main()
+	__fov_test()
+end
+
+--------------------------------------------------------------------
+
+main()
+
+--------------------------------------------------------------------
